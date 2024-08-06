8dacbe580efc681acabc4c39ea981aef1608ceec
Eduardo Nathan <143146733+HadawardA7X@users.noreply.github.com>
2024-08-05 22:36:59 -0300

    Adding a new project
    Uma calculadora simples em C#

---
+++ MyFirstProgram/Program.cs
@@ -0,0 +1,51 @@
+﻿do
+{
+    double num1 = 0;
+    double num2 = 0;
+    double res = 0;
+
+    Console.WriteLine("-----------");
+    Console.WriteLine("CALCULADORA");
+    Console.WriteLine("-----------");
+
+    Console.Write("Digite o primeiro número:");
+    num1 = Convert.ToDouble(Console.ReadLine());
+    Console.Write("Digite o segundo número: ");
+    num2 = Convert.ToDouble(Console.ReadLine());
+
+    Console.WriteLine("Digite um operador: ");
+    Console.WriteLine("\t+ : Adicionar");
+    Console.WriteLine("\t- : Subtrair");
+    Console.WriteLine("\t* : Multiplicar");
+    Console.WriteLine("\t/ : Dividir");
+    Console.Write("Selecione uma opção: ");
+
+    switch (Console.ReadLine())
+    {
+        case "+":
+            res = num1 + num2;
+            Console.WriteLine($"Resultado: {num1} + {num2} = {res}");
+            break;
+        case "-":
+            res = num1 - num2;
+            Console.WriteLine($"Resultado: {num1} - {num2} = {res}");
+            break;
+        case "*":
+            res = num1 * num2;
+            Console.WriteLine($"Resultado: {num1} * {num2} = {res}");
+            break;
+        case "/":
+            res = num1 / num2;
+            Console.WriteLine($"Resultado: {num1} / {num2} = {res}");
+            break;
+        default:
+            Console.WriteLine("Isso não é um operador válido.");
+            break;
+    }
+    Console.Write("Gostaria de usar novamente? (Y/N): ");
+
+} while (Console.ReadLine().ToUpper() == "Y");
+
+Console.WriteLine("Obrigado por usar meu programa!");
+
+Console.ReadKey();
